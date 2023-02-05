8611a6bbb49fc0e998ff9e82acf663ea8e530adc
Kunal Pal <kunal17265@gmail.com>
2023-02-05 19:53:10 +0530

    Populated the famous cities section

--- script/app.js
+++ script/app.js
@@ -39,3 +39,61 @@ submit.addEventListener("click", (e) => {
 });
 
 getWeather("Bengaluru");
+
+//To convert the time from date Unix timestamp
+var getTime = (val) => {
+  var timestamp = val;
+  var date = new Date(timestamp * 1000);
+
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+
+  return hours + ":" + minutes + ":" + seconds;
+};
+
+const fillDets = async () => {
+  var details = {
+    temp: document.getElementsByClassName("temp"),
+    // cloud_pct: document.getElementsByClassName("cloud_pct"),
+    // feels_like: document.getElementsByClassName("feels_like"),
+    humidity: document.getElementsByClassName("humidity"),
+    max_temp: document.getElementsByClassName("max_temp"),
+    min_temp: document.getElementsByClassName("min_temp"),
+    sunrise: document.getElementsByClassName("sunrise"),
+    sunset: document.getElementsByClassName("sunset"),
+    wind_speed: document.getElementsByClassName("wind_speed"),
+    // wind_degrees: document.getElementsByClassName("wind_degrees"),
+  };
+
+  var cities = ["Shanghai", "New York", "London", "Tokyo", "Mumbai"];
+
+  for (let i = 0; i < 5; i++) {
+    await fetch(
+      "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" +
+        cities[i],
+      options
+    )
+      .then(async (res) => await res.json())
+      .then((res) => {
+        details.temp[i].innerHTML = res.temp;
+        console.log(res);
+        // details.cloud_pct[i].innerHTML = res.cloud_pct;
+        // details.feels_like[i].innerHTML = res.feels_like;
+        details.humidity[i].innerHTML = res.humidity;
+        details.min_temp[i].innerHTML = res.min_temp;
+        details.max_temp[i].innerHTML = res.max_temp;
+        details.wind_speed[i].innerHTML = res.wind_speed;
+        // details.wind_degrees[i].innerHTML = res.wind_degrees;
+        details.sunrise[i].innerHTML = getTime(res.sunrise);
+        details.sunset[i].innerHTML = getTime(res.sunset);
+      })
+      .catch((err) => console.error(err));
+  }
+};
+
+fillDets();
+
+var timestamp = 1675616146;
+var date = new Date(timestamp * 1000);
+console.log(date);
